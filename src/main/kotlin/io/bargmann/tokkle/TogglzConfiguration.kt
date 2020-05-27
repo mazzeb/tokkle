e1151fcfa4de994de5754efd7aa5b82aac41405e
Matthias Bargmann <matthias@bargmann.io>
2020-05-27 06:20:25 +0200

    add CachingStaterepository

--- src/main/kotlin/io/bargmann/tokkle/TogglzConfiguration.kt
+++ src/main/kotlin/io/bargmann/tokkle/TogglzConfiguration.kt
@@ -2,33 +2,37 @@ package io.bargmann.tokkle
 
 import com.mongodb.MongoClient
 import com.mongodb.WriteConcern
-import org.springframework.boot.autoconfigure.mongo.MongoProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.togglz.core.context.StaticFeatureManagerProvider
 import org.togglz.core.manager.FeatureManager
 import org.togglz.core.manager.FeatureManagerBuilder
 import org.togglz.core.repository.StateRepository
+import org.togglz.core.repository.cache.CachingStateRepository
 import org.togglz.core.spi.FeatureProvider
 import org.togglz.core.user.UserProvider
 import org.togglz.kotlin.EnumClassFeatureProvider
 import org.togglz.mongodb.MongoStateRepository
 
 @Configuration
-@EnableConfigurationProperties(MongoProperties::class)
-class TogglzConfiguration(private val mongoProperties: MongoProperties) {
+class TogglzConfiguration {
+
+    companion object {
+        private const val TOGGLE_CACHE_TIMEOUT = 3000L
+    }
 
     @Bean
     fun featureProvider() = EnumClassFeatureProvider(Features::class.java)
 
     @Bean
     fun stateRepository(mongoClient: MongoClient): StateRepository {
-        return MongoStateRepository
-                .newBuilder(mongoClient, "someDatabase")
-                .collection("features")
-                .writeConcern(WriteConcern.ACKNOWLEDGED)
-                .build()
+        return CachingStateRepository(
+                MongoStateRepository
+                        .newBuilder(mongoClient, "someDatabase")
+                        .collection("features")
+                        .writeConcern(WriteConcern.ACKNOWLEDGED)
+                        .build(),
+                TOGGLE_CACHE_TIMEOUT)
     }
 
     @Bean
